6f3987dd160f3283ab0aacd0ed2cade4f1fb6b7f
Jan Eglinger <jan.eglinger@fmi.ch>
2026-01-05 15:45:30 +0100

    Parameterize test on companion.ome files

--- tests/test_companion.py
+++ tests/test_companion.py
@@ -3,16 +3,25 @@ import pytest
 from ome_dataarray.companion import CompanionFile
 from pathlib import Path
 
-@pytest.fixture
-def companion_file_path():
-    return Path(__file__).parent / "resources" / "20250612_6pp7_1_gain50_300ms_488_10_640_2_10.companion.ome"
 
+@pytest.mark.parametrize(
+    "companion_file_name,expected_sum",
+    [
+        ("20250910_test4ch_2roi_3z_1_sg1.companion.ome", 9061879471),
+        ("20250910_test4ch_2roi_3z_1_sg2.companion.ome", 10522218363),
+    ],
+)
+def test_from_file_vv7(companion_file_name, expected_sum):
+    folder = Path(__file__).parent / "resources" / "20250910_VV7-0-0-6-ScanSlide"
+    companion_file_path = folder / companion_file_name
 
-def test_from_file(companion_file_path):
     assert companion_file_path.exists(), "companion.ome test file does not exist"
 
     companion_file = CompanionFile(companion_file_path)
     data_array = companion_file.get_dataarray()
     assert data_array is not None
-    assert data_array.shape == (100, 3, 1, 512, 512)  # (T, C, Z, Y, X)
+    assert data_array.shape == (1, 4, 3, 512, 512)  # (T, C, Z, Y, X)
     assert data_array.dtype == 'uint16'
+    assert list(data_array.dims) == ["t", "c", "z", "y", "x"]
+    # Data integrity check
+    assert data_array.sum().compute() == expected_sum
